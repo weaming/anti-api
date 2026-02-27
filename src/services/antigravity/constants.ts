a303b6bbbcba294579a440c2063b4029a7efa958
weaming <garden.yuen@gmail.com>
2026-02-27 21:01:27 +0800

    update to 1.19.6

--- src/services/antigravity/constants.ts
+++ src/services/antigravity/constants.ts
@@ -2,5 +2,5 @@
  * Antigravity Service Constants
  */
 
-export const ANTIGRAVITY_VERSION = "1.19.5"
+export const ANTIGRAVITY_VERSION = "1.19.6"
 export const DEFAULT_ANTIGRAVITY_USER_AGENT = `antigravity/${ANTIGRAVITY_VERSION} windows/amd64`
